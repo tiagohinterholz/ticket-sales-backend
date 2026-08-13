4683e5ada1d371d7fdaa3f275cc32420c530024f
Tiago Hinterholz <fh.tiago@gmail.com>
2026-08-12 22:45:13 -0300

    chore(db): configure Alembic, DB session and base mixins

---
+++ app/models/mixins.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class UUIDPKMixin:
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
